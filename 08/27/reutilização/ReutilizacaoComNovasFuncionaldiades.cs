b9ed92cdce13ae2dbd1c348ab104215e7cf88f0c
JoseMaksud <jf9718830@gmail.com>
2024-09-30 19:50:47 -0300

    Novas funcionalidades implementadas.

--- 08/27/reutilização/ReutilizacaoComNovasFuncionaldiades.cs
+++ 08/27/reutilização/ReutilizacaoComNovasFuncionaldiades.cs
@@ -6,47 +6,46 @@ public class Program
     public static void Main(string[] args)
     {
         // Software de validação de senha
-        Console.WriteLine("Digite uma senha:");
-        string senha = ValidarSenha(Console.ReadLine());
-
-        Console.WriteLine("\nConfirme a senha:");
-        string confirmaSenha = ValidarSenha(Console.ReadLine());
-
+        Console.Write("Digite uma senha:");
+        String senha = ValidarSenha(Console.ReadLine());
+        
+        Console.Write("\nConfirme a senha:");
+        String confirmaSenha = ValidarSenha(Console.ReadLine());    
+        
+        // faz a validação
         bool igual = ComparaTexto(senha, confirmaSenha);
-
+       
         if (igual)
             Console.WriteLine("\nArmazenando dados!");
         else
-            Console.WriteLine("\nRefaca o processo!");
+            Console.WriteLine("\nRefaça o processo");
     }
-
-
+    
     private static string ValidarSenha(string texto)
     {
         if (texto.Length < 8)
-            Console.WriteLine("Tamanho invalido!");
+            Console.WriteLine("\nTamanho inválido!");
         if (!texto.Any(char.IsDigit))
-            Console.WriteLine("Nao possui numeros!");
+            Console.WriteLine("\nNão possui números!");
         if (!texto.Any(char.IsUpper))
-            Console.WriteLine("Nao possui letra maiuscula!");
-
+            Console.WriteLine("\nNão possui letras maiúsculas!");
+        if (!texto.Any(ch => !char.IsLetterOrDigit(ch)))
+            Console.WriteLine("\nNão possui caracteres especiais!");
 
         return texto;
     }
-
+    
     private static bool ComparaTexto(string senha, string confirmaSenha)
     {
-        if (senha == confirmaSenha)
+        if (senha.Equals(confirmaSenha, StringComparison.OrdinalIgnoreCase))
         {
             Console.WriteLine("\nSenhas conferem!");
             return true;
         }
         else
         {
-            Console.WriteLine("\nSenhas nao conferem!");
+            Console.WriteLine("\nSenhas não conferem!");
             return false;
         }
-
     }
-
 }
